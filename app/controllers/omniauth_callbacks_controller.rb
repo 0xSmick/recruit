f9c5f9461e4ec64b4e4af00560dd30916517c3e1
undeadsushi <sheldon.smickley@gmail.com>
2014-05-29 23:40:27 -0400

    updated homepage and omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController   
   def linkedin
     user = User.from_omniauth(request.env["omniauth.auth"])
+
     if user.persisted?
-      sign_in_and_redirect user, notice: "Signed in!"
+      flash.notice = "Signed In"
+      sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
